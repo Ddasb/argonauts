aee5b87263cbb26d162dab478f9f891f04ffa908
Ddasb <damien.bregieiro@gmail.com>
2023-02-18 22:20:28 +0100

    push

--- src/main/java/com/kiuseii/argonauts/init/ArgonautsItems.java
+++ src/main/java/com/kiuseii/argonauts/init/ArgonautsItems.java
@@ -11,6 +11,57 @@ import net.minecraftforge.registries.RegistryObject;
 public class ArgonautsItems {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Argonauts.MOD_ID);
 
-  public static final RegistryObject<Item> ADAMANTITE_INGOT = ITEMS.register("adamantite_ingot",
+  public static final RegistryObject<Item> ADAMANTITE = ITEMS.register("adamantite",
       () -> new Item(new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+
+  public static final RegistryObject<Item> DIR_ADAMANTITE = ITEMS.register("dir_adamantite",
+      () -> new Item(new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+
+  public static final RegistryObject<Item> SEIROS = ITEMS.register("seiros",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+  public static final RegistryObject<Item> THOUSAND_YEAR_TREE_SAP = ITEMS.register("thousand_year_tree_sap",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+  public static final RegistryObject<Item> BLOOD_ONYX = ITEMS.register("blood_onyx",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> MITHRIL = ITEMS.register("mithril",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> NOSTEEL = ITEMS.register("nosteel",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> ORICALCHUM = ITEMS.register("oricalchum",
+      () -> new Item(new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+
+  public static final RegistryObject<Item> UNDER_CORAL = ITEMS.register("under_coral",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> HOLYDITE = ITEMS.register("holydite",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> OATHTREE_WALNUT = ITEMS.register("oathree_walnut",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+  public static final RegistryObject<Item> HEROIC_ALLOY = ITEMS.register("heroic_alloy",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+  public static final RegistryObject<Item> VALMARTH = ITEMS.register("valmarth",
+      () -> new Item(new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+
+  public static final RegistryObject<Item> DAMASCUS = ITEMS.register("damascus",
+      () -> new Item(new Item.Properties().rarity(Rarity.EPIC).fireResistant()));
+
+  public static final RegistryObject<Item> LIGHT_METAL = ITEMS.register("light_metal",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> WHITE_WOOD = ITEMS.register("white_wood",
+      () -> new Item(new Item.Properties().rarity(Rarity.COMMON).fireResistant()));
+
+  public static final RegistryObject<Item> HIHIIROKANE = ITEMS.register("hihiirokane",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+  public static final RegistryObject<Item> MAGIC_CRYSTAL = ITEMS.register("magic_crystal",
+      () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
 }
--- src/main/java/com/kiuseii/argonauts/init/ArgonautsTabs.java
+++ src/main/java/com/kiuseii/argonauts/init/ArgonautsTabs.java
@@ -14,11 +14,34 @@ public class ArgonautsTabs {
 
   @SubscribeEvent
   public static void registerArgonautsTabs(CreativeModeTabEvent.Register event) {
+    event.registerCreativeModeTab(new ResourceLocation("argonauts_blocks"), builder -> builder
+        .title(Component.translatable("itemGroup.argonauts.blocks"))
+        .icon(() -> new ItemStack(ArgonautsItems.ADAMANTITE.get()))
+        .displayItems((featuredFlag, output, operator) -> {
+        }));
+
     event.registerCreativeModeTab(new ResourceLocation("argonauts_items"), builder -> builder
         .title(Component.translatable("itemGroup.argonauts.items"))
-        .icon(() -> new ItemStack(ArgonautsItems.ADAMANTITE_INGOT.get()))
+        .icon(() -> new ItemStack(ArgonautsItems.ADAMANTITE.get()))
         .displayItems((featuredFlag, output, operator) -> {
-          output.accept(ArgonautsItems.ADAMANTITE_INGOT.get());
+          output.accept(ArgonautsItems.ADAMANTITE.get());
+          output.accept(ArgonautsItems.BLOOD_ONYX.get());
+          output.accept(ArgonautsItems.DAMASCUS.get());
+          output.accept(ArgonautsItems.DIR_ADAMANTITE.get());
+          output.accept(ArgonautsItems.HEROIC_ALLOY.get());
+          output.accept(ArgonautsItems.HIHIIROKANE.get());
+          output.accept(ArgonautsItems.HOLYDITE.get());
+          output.accept(ArgonautsItems.LIGHT_METAL.get());
+          output.accept(ArgonautsItems.MITHRIL.get());
+          output.accept(ArgonautsItems.NOSTEEL.get());
+          output.accept(ArgonautsItems.OATHTREE_WALNUT.get());
+          output.accept(ArgonautsItems.ORICALCHUM.get());
+          output.accept(ArgonautsItems.SEIROS.get());
+          output.accept(ArgonautsItems.THOUSAND_YEAR_TREE_SAP.get());
+          output.accept(ArgonautsItems.UNDER_CORAL.get());
+          output.accept(ArgonautsItems.VALMARTH.get());
+          output.accept(ArgonautsItems.WHITE_WOOD.get());
+          output.accept(ArgonautsItems.MAGIC_CRYSTAL.get());
         }));
   }
 }
